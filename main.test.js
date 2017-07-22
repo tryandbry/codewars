a88bcb9d12d5c80410be55f31e7544b9eb4a517d
bryan <bryan@localhost.localdomain>
2017-07-22 16:00:49 -0500

    setting up unit testing

---
+++ main.test.js
@@ -0,0 +1,14 @@
+import {expect} from 'chai';
+import {spy} from 'sinon';
+
+
+describe('unit tests',function(){
+  /*
+  beforeEach(function(){
+  });
+  */
+
+  it('test 1',function(){
+    expect(1).to.equal(1);
+  });
+});
