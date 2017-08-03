d35a4a8f77cf0ff199498f5df676b22277051910
bryan <bryan@localhost.localdomain>
2017-08-03 11:59:22 -0500

    1.1 passing

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 
 export const isUnique = (str) => {
+  let hash = {};
+  let arr = str.split('');
+  for(let i in str.split('')){
+    if(hash[arr[i]] === true){
+      return false;
+    }
+    else hash[arr[i]] = true;
+  }
   return true;
 }
