a6ccacc9ec17e417f77ead5a570685b70e705c9a
bryan <bryan@localhost.localdomain>
2017-08-25 13:24:24 -0500

    problem 1.1 passing

--- ch2.js
+++ ch2.js
@@ -1,2 +1,151 @@
-export const dedupeList = (head) => {
+
+/* BEGIN function class definition
+ *
+export function Node(val,next,prev){
+  this.val = val;
+  this.next = next;
+  this.prev = prev;
 }
+
+export function LinkedList(val){
+  let newNode = new Node(val);
+  this.head = newNode;
+  this.tail = newNode;
+  this.length = 1;
+}
+
+LinkedList.prototype.pushToHead = function(val){
+  let newNode = new Node(val,this.head);
+  this.head.prev = newNode;
+  this.head = newNode;
+  this.length++;
+  return newNode;
+}
+
+LinkedList.prototype.pushToTail = function(val){
+  let newNode = new Node(val,undefined,this.tail);
+  this.tail.next = newNode;
+  this.tail = newNode;
+  this.length++;
+  return newNode;
+}
+
+LinkedList.prototype.popNode = function(node){
+  this.length--;
+  if(node === this.head) {
+    this.head.next.prev = null;
+    this.head = this.head.next;
+    return node;
+  }
+  else if(node === this.tail) {
+    this.tail.prev.next = null;
+    this.tail = this.tail.prev;
+    return node;
+  }
+  else {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+    return node;
+  }
+}
+ *
+ * END function class definition */
+export class Node {
+  constructor(val,next,prev){
+    this.val = val;
+    this.next = next;
+    this.prev = prev;
+  }
+}
+
+export class LinkedList {
+  constructor(val){
+    this.head = new Node(val);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  pushToHead(val) {
+    let newNode = new Node(val);
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+    return this.head;
+  }
+
+  pushToTail(val) {
+    let newNode = new Node(val);
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this.tail;
+  }
+
+  popNode(node) {
+    if(node === this.head) {
+      this.head.next.prev = null;
+      this.head = this.head.next;
+    }
+    else if(node === this.tail) {
+      this.tail.prev.next = null;
+      this.tail = this.tail.prev;
+    }
+    else {
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+    }
+    this.length--;
+    return node;
+  }
+}
+
+
+export const dedupeList = (list) => {
+  let hash = {};
+  let currentNode = list.head;
+  while(currentNode){
+    if(hash[currentNode.val]) list.popNode(currentNode);
+    else hash[currentNode.val] = true;
+
+    currentNode = currentNode.next;
+  }
+}
+
+export const dedupeListNoBuffer = (list) => {
+  let currentNode = list.head;
+  while(currentNode){
+    let backRunner = currentNode.prev;
+    while(backRunner) {
+      if(backRunner.val === currentNode.val) {
+        list.popNode(currentNode);
+        break;
+      }
+      backRunner = backRunner.prev;
+    }
+    currentNode = currentNode.next;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ch2.test.js
+++ ch2.test.js
@@ -1,6 +1,140 @@
 import {expect} from 'chai';
 //import {spy} from 'sinon';
-import * as codewars from './main';
+import * as codewars from './ch2';
 
-describe('dedupeList',function(){
+describe('Chapter 2: Linked List',function(){
+  describe('Base LinkedList functionality',function(){
+    let list;
+    beforeEach(function(){
+      list = new codewars.LinkedList(1);
+    });
+
+    it('instantiates',function(){
+      expect(list).to.be.an.instanceOf(codewars.LinkedList);
+    });
+
+    it('stores a value',function(){
+      expect(list.head.val).to.equal(1);
+    });
+
+    it('adds to head',function(){
+      expect(list.head.val).to.equal(1);
+      list.pushToHead(2);
+      expect(list.head.val).to.equal(2);
+      list.pushToHead(3);
+      expect(list.head.val).to.equal(3);
+      /*
+      expect(list.next.val).to.equal(2);
+      expect(list.next.next.val).to.equal(3);
+      */
+    });
+
+    it('adds to tail',function(){
+      expect(list.head.val).to.equal(1);
+      list.pushToTail(2);
+      expect(list.tail.val).to.equal(2);
+      list.pushToTail(3);
+      expect(list.tail.val).to.equal(3);
+    });
+
+    it('removes nodes from the middle of the list',function(){
+      let a = list.pushToTail(2);
+      list.pushToTail(3);
+      list.pushToTail(4);
+      list.popNode(a);
+      expect(list.head.val).to.equal(1);
+      expect(list.head.next.val).to.equal(3);
+      expect(list.head.next.next.val).to.equal(4);
+    });
+
+    it('removes nodes from the head',function(){
+      list.pushToTail(2);
+      list.popNode(list.head);
+      expect(list.head.val).to.equal(2);
+    });
+
+    it('removes nodes from the tail',function(){
+      list.pushToHead(2);
+      list.popNode(list.tail);
+      expect(list.tail.val).to.equal(2);
+    });
+
+    it('tracks list length',function(){
+      expect(list.length).to.equal(1);
+      list.pushToTail(2);
+      expect(list.length).to.equal(2);
+      list.pushToHead(3);
+      expect(list.length).to.equal(3);
+      list.popNode(list.head);
+      expect(list.length).to.equal(2);
+      list.popNode(list.tail);
+      expect(list.length).to.equal(1);
+    });
+  });
+
+  describe('Problem 1.1',function(){
+    describe('dedupeList',function(){
+      let list;
+      beforeEach(function(){
+        list = new codewars.LinkedList(1);
+      });
+
+      it('removes nodes with duplicate values',function(){
+        list.pushToTail(1);
+        list.pushToTail(1);
+        list.pushToTail(1);
+        list.pushToTail(1);
+        codewars.dedupeList(list);
+        expect(list.length).to.equal(1);
+      });
+    });
+
+    describe('dedupeListNoBuffer',function(){
+      /*
+       * Note: same test(s) as dedupeList
+       */
+      let list;
+      beforeEach(function(){
+        list = new codewars.LinkedList(1);
+      });
+
+      it('removes nodes with duplicate values',function(){
+        list.pushToTail(1);
+        list.pushToTail(1);
+        list.pushToTail(1);
+        list.pushToTail(1);
+        codewars.dedupeListNoBuffer(list);
+        expect(list.length).to.equal(1);
+      });
+    });
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
